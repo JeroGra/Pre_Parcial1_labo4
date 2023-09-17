e536252946faf1fc211045f24db5f01079e5654e
jero <jerog0@gmail.com>
2023-09-17 19:37:00 -0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { ActorListadoComponent } from './componentes/actor-listado/actor-listado
 import { PeliculaListadoComponent } from './componentes/pelicula-listado/pelicula-listado.component';
 import { TablaPeliculaComponent } from './componentes/tabla-pelicula/tabla-pelicula.component';
 import { DetallePeliculaComponent } from './componentes/detalle-pelicula/detalle-pelicula.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,10 @@ import { DetallePeliculaComponent } from './componentes/detalle-pelicula/detalle
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'preparcial1-61325',
+    appId: '1:437248129215:web:41e292a48a6049bd122e0a',
+    storageBucket: 'preparcial1-61325.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCerVZVCSMRG495jgRAJ8GkoIDvjXgFsbw',
+    authDomain: 'preparcial1-61325.firebaseapp.com',
+    messagingSenderId: '437248129215',
+  },
+};
